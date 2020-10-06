e0b36d8820461afb660880b74dffeda6bf9be38c
breno500as <breno500as@gmail.com>
2020-10-06 17:22:32 -0300

    istio

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -39,3 +39,17 @@ CREATE TABLE IF NOT EXISTS oauth_refresh_token (
 CREATE TABLE IF NOT EXISTS oauth_code (
   code VARCHAR(256), authentication bytea
 );
+
+CREATE TABLE IF NOT EXISTS users (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  username VARCHAR(256) NOT NULL,
+  password VARCHAR(256) NOT NULL,
+  enabled TINYINT(1),
+  UNIQUE KEY unique_username(username)
+);
+
+CREATE TABLE IF NOT EXISTS authorities (
+  username VARCHAR(256) NOT NULL,
+  authority VARCHAR(256) NOT NULL,
+  PRIMARY KEY(username, authority)
+);
