d248d92049e9f9c87fd8acd1e529bedf5bc7c516
breno500as <breno500as@gmail.com>
2020-10-07 19:57:43 -0300

    istio

--- src/main/java/com/istio/pet/shop/security/controller/LoginController.java
+++ src/main/java/com/istio/pet/shop/security/controller/LoginController.java
@@ -1,12 +1,15 @@
 package com.istio.pet.shop.security.controller;
 
 import java.util.Arrays;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.istio.pet.shop.security.entity.Usuario;
@@ -22,6 +25,12 @@ public class LoginController {
 	@PostMapping
 	public ResponseEntity<String> login(@RequestBody Usuario usuario) {
 		return ResponseEntity.ok(this.jwtService.buildAuthToken(1L, "breno", "breno500as@gmail.com", Arrays.asList("ADMIN")));
+	}
+	
+	@GetMapping
+	public ResponseEntity<String> verify(@RequestParam(name = "token") String token){
+		final String usuario = this.jwtService.getDadosUsuarioToken(token).stream().map(u -> u).collect(Collectors.joining());
+		return ResponseEntity.ok(usuario);
 	}
 
 }
